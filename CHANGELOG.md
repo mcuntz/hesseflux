89fae563f8de698923d9772d24fa7deecb3bea12
Matthias Cuntz <mc@macu.de>
2020-05-18 01:28:46 +0200

    Finished setup in repositories with all dependencies, TWINE password, etc.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,9 @@
 
 All notable changes after its initial release in May 2020 (v2.0) are documented in this file.
 
+### v2.0.2 (May 2020)
+    - Finished setup in repositories with all dependencies, TWINE password, etc.
+
 ### v2.0.1 (May 2020)
     - Add more requirements for readthedocs, coverall, etc.
 
--- hesseflux/version.py
+++ hesseflux/version.py
@@ -11,9 +11,10 @@ Released under the MIT License; see LICENSE file for details.
 * Written 2017 by Matthias Cuntz (mc (at) macu (dot) de)
 * v2.0, format and docu useable with PyPI, Apr 2020, Matthias Cuntz
 * v2.0.1, more requirements for readthedocs, coveralls, etc., May 2020, Matthias Cuntz
+* v2.0.2, finished setup with all dependencies, setting passwords, etc., May 2020, Matthias Cuntz
 
 .. moduleauthor:: Matthias Cuntz
 """
 
 __author__  = "Matthias Cuntz"
-__version__ = "2.0.1"
+__version__ = "2.0.2"
